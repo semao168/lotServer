0816745ee814eafae6d16fcb46b3be54f6ec732d
Heidis <heidis168@proton.me>
2024-06-22 15:58:10 +0800

    last

--- TCPAspeed/tcpa.sh
+++ TCPAspeed/tcpa.sh
@@ -8,9 +8,7 @@ export LANG=en_US.UTF-8
 yum -y install epel-release net-tools bzip2
 
 curl -o /tmp/tcpa_packets_180619_1151.tar.bz2 https://raw.githubusercontent.com/semao168/lotServer/main/TCPAspeed/tcpa_packets_180619_1151.tar.bz2
-chmod +x /etc/rc.d/rc.local
-cat>>/etc/rc.local<<EOF
-####tcpa####
+
 cd /tmp/
 tar jxvf tcpa_packets_180619_1151.tar.bz2
 cd tcpa_packets
@@ -20,11 +18,9 @@ sh start.sh
 lsmod|grep tcpa
 rm -f /tmp/tcpa_packets_180619_1151.tar.bz2
 rm -rf /tmp/tcpa_packets
-sed -i '/####tcpa####/','/####tcpa####/d' /etc/rc.local
-####tcpa####
-EOF
-
-rpm -ivh https://raw.githubusercontent.com/semao168/lotServer/TCPAspeed/main/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
+chmod +x /etc/rc.d/rc.local
+echo "/usr/local/storage/tcpav2/start.sh" >> /etc/rc.d/rc.local
+rpm -ivh https://raw.githubusercontent.com/semao168/lotServer/main/TCPAspeed/kernel-3.10.0-693.5.2.tcpa06.tl2.x86_64.rpm --force
 echo "内核安装完毕,3秒后将自动重启..."
 echo "重启后安装自动完成,lsmod|grep tcpa查看是否开启成功."
 sleep 3
